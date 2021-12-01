84186b13d7be91dac373ab20f5bb2b8dab447f10
NjokanmaDonald <njokanmadon@gmail.com>
2021-12-01 15:55:10 +0100

    make it better

--- index.js
+++ index.js
@@ -38,6 +38,9 @@ app.post("/api/upload", upload.single("file"), (req,res) =>{
     res.status(200).json("File has been uploaded")
 })
 
+app.get("/", (req,res) => {
+    res.send("Hello to my api")
+})
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
